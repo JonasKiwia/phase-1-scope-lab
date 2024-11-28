efa3fed6c07bff11ac4ba70835012851ab047223
JonasKiwia <jonas.kiwia@student.moringaschool.com>
2024-11-29 00:01:29 +0300

    Implement customerName and related functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// 1.  customerName Variable in global scope 
+var customerName = 'bob';
+
+// 2. Define upperCaseCustomerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Define setBestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// 4. Define overwriteBestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// 5. Declare leastFavoriteCustomer and define changeLeastFavoriteCustomer
+const leastFavoriteCustomer = 'initial customer';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new customer'; // This will throw an error
+}
